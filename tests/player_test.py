71e2fc3ad804379d8ee7d22eaf6006622303af54
klamb95 <klamb1995@gmail.com>
2021-04-08 14:34:14 +0100

    second player working

--- tests/player_test.py
+++ tests/player_test.py
@@ -7,8 +7,14 @@ class TestPlayer(unittest.TestCase):
         self.player_1 = Player("Steven", "Rock")
         self.player_2 = Player("Kieran", "Paper")
 
-    def test_player_name(self):
+    def test_player_name_1(self):
         self.assertEqual("Steven", self.player_1.name)
 
-    def test_player_choice(self):
+    def test_player_choice_1(self):
         self.assertEqual("Rock", self.player_1.choice)
+
+    def test_player_name_2(self):
+        self.assertEqual("Kieran", self.player_2.name)
+
+    def test_player_choice_2(self):
+        self.assertEqual("Paper", self.player_2.choice)
